38dbf101b59c171c0273c505f92963d3e107c63d
Puru1302 <bodireddypurush1212@gmail.com>
2026-07-20 02:11:43 +0530

    Configured Railway MySQL

--- vendor_credit_system/settings.py
+++ vendor_credit_system/settings.py
@@ -49,16 +49,16 @@ WSGI_APPLICATION = 'vendor_credit_system.wsgi.application'
 
 # MySQL Database Configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("vendor_credit_db"),
-        'USER': os.environ.get("root"),
-        'PASSWORD': os.environ.get("rPFcstlStzVaullaxVJmttHwsrPBPfrN"),
-        'HOST': os.environ.get("tokaido.proxy.rlwy.net"),
-        'PORT': os.environ.get("31930"),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "railway",
+        "USER": "root",
+        "PASSWORD": "rPFcstlStzVaullaxVJmttHwsrPBPfrN",
+        "HOST": "tokaido.proxy.rlwy.net",
+        "PORT": "31930",
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
